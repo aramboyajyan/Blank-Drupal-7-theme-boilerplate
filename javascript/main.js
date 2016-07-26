a156f47047f44a69c65e6f040a561f2ed2ebbe60
Aram Boyajyan <dev@aram.cz>
2016-07-26 16:45:37 +0200

    adding initial list of files

---
+++ javascript/main.js
@@ -0,0 +1,17 @@
+/**
+ * @file
+ * Common theme JS.
+ *
+ * Created by: Topsitemakers
+ * http://www.topsitemakers.com/
+ */
+(function ($) {
+  Drupal.behaviors.BoilerplateTheme = {
+    attach: function (context, settings) {
+
+      console.log('boilerplate theme JS loaded');
+
+    }
+  };
+
+})(jQuery);
